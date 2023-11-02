cd4b4421fd9d9abd4ab26d82a27ad9f6565053dc
JackStras <jackstrasbaugh@yahoo.com>
2023-11-02 17:25:44 -0400

    Started logo function, added if statement to choose shape class

--- index.js
+++ index.js
@@ -7,19 +7,39 @@ const {Triangle, Square, Circle} = require("./lib/shapes")
 const prompts = [
     {
         type: "input",
-        name: "colorInput",
-        message: "Please enter a color. You may use hexadecimal Number."
+        name: "shapeColor",
+        message: "Please enter a color for the logo. You may use hexadecimal number."
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Please enter a color for the text. You may use hexadecimal number."
     },
     {
         type: "list",
         name: "shapeInput",
-        message: "Please select a shape."
+        message: "Please select a shape.",
         choices: ["Triangle", "Square", "Circle"]
     },
     {
         type: "maxlength-input",
         name: "textInput",
-        message: "Select up to 3 characters for your logo."
+        message: "Select up to 3 characters for your logo.",
         maxLength: 3
     }
 ]
+
+function makeLogo(file, data) {
+    if (data.shapeInput == "Triangle") {
+        const shape = new Triangle()
+        const logoText = `<text x="109" y="135" font-size="45" fill="${data.textColor}">${data.textInput}</text>`
+    } else if (data.shapeInput == "Square") {
+        const shape = new Square()
+        const logoText = `<text x="109" y="115" font-size="45" fill="${data.textColor}">${data.textInput}</text>`
+    } else {
+        const shape = new Circle()
+        const logoText = `<text x="109" y="115" font-size="45" fill="${data.textColor}">${data.textInput}</text>`
+    }
+
+
+}
