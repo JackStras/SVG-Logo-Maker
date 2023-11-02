c505d44b7cce61ebcce15c01c9cd908363f7935b
JackStras <jackstrasbaugh@yahoo.com>
2023-11-02 16:48:47 -0400

    Created array of prompts for user input

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const inquirer = require("inquirer")
 const maxLengthInputPrompt = require("inquirer-maxlength-input-prompt")
 const fs = require("fs")
-inquirer.registerPrompt('maxlength-input', maxLengthInputPrompt)
+inquirer.registerPrompt("maxlength-input", maxLengthInputPrompt)
 const {Triangle, Square, Circle} = require("./lib/shapes")
+
+const prompts = [
+    {
+        type: "input",
+        name: "colorInput",
+        message: "Please enter a color. You may use hexadecimal Number."
+    },
+    {
+        type: "list",
+        name: "shapeInput",
+        message: "Please select a shape."
+        choices: ["Triangle", "Square", "Circle"]
+    },
+    {
+        type: "maxlength-input",
+        name: "textInput",
+        message: "Select up to 3 characters for your logo."
+        maxLength: 3
+    }
+]
